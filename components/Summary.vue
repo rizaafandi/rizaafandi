95a891d10946c66d028a1033ee078b79a3dd069c
Riza Afandi <44855641+rizaafandi@users.noreply.github.com>
2023-10-23 16:12:33 +0700

    Update Summary.vue

--- components/Summary.vue
+++ components/Summary.vue
@@ -11,14 +11,5 @@
       projects that enhance user experiences while optimizing performance and
       scalability
     </p>
-    <p class="text-gray dark:text-gray-400 text-lg">
-      With strong foundations in JavaScript, HTML, CSS and frameworks such as
-      Vue.js, React.js and ASP.NET. I am committed to crafting dynamic and
-      responsive web solutions. Aspiring to contribute my expertise in
-      full-stack development to a collaborative team, I am eager to engage in
-      challenging projects that allow me to further enhance my skills to create
-      meaningful software, and contribute to the evolution of cutting-edge
-      digital technologies
-    </p>
   </div>
 </template>
